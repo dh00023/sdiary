bd7615962e24063eea1698d6e681711a6d20219d
hyunkyung <khc0275@naver.com>
2017-08-07 08:28:34 +0000

    post 모델

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807063146) do
+ActiveRecord::Schema.define(version: 20170807075955) do
 
   create_table "diaries", force: :cascade do |t|
     t.string   "category"
     t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
