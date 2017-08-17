512fe5d94dd810a15d83d85677d1391ec8d3a9d9
hyunkyung <khc0275@naver.com>
2017-08-17 11:27:35 +0000

    댓글 user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-
+    @comment.user_id = current_user.id
+    
     if @comment.save
       respond_to do |format|
         format.js
@@ -24,7 +25,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content,:user_id)
   end
   
 end 
