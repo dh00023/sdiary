1f3e6a37096f4ed08b3e2af585cfc2755e86ed30
dahye Jeong <wjdekgp1750@naver.com>
2017-08-19 04:16:11 +0900

    에러

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
     has_many :comments
+    validates :category, presence: { message: "카테고리을 입력해주세요." }
+    validates :title, presence: { message: "카테고리을 입력해주세요." }
+    validates :content, presence: { message: "카테고리을 입력해주세요." }
 end
